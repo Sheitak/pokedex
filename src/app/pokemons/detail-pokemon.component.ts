5eed18ed501d2f59b90726b2681611e9c942a084
Quentin <goupilloux.qm@orange.fr>
2020-03-17 22:12:23 +0100

    Add pokemonsService in Pokedex-app

--- src/app/pokemons/detail-pokemon.component.ts
+++ src/app/pokemons/detail-pokemon.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Pokemon } from './pokemon';
-import { POKEMONS } from './mock-pokemons';
+import { PokemonsService } from './pokemons.service';
 
 @Component({
     selector: 'detail-pokemon',
@@ -9,24 +9,26 @@ import { POKEMONS } from './mock-pokemons';
 })
 export class DetailPokemonComponent implements OnInit {
 
-    pokemons: Pokemon[] = null;
     pokemon: Pokemon = null;
 
-    constructor(private route: ActivatedRoute, private router: Router) {}
+    constructor(
+      private route: ActivatedRoute,
+      private router: Router,
+      private pokemonsService: PokemonsService
+      ) { }
+
 
     ngOnInit(): void {
-        this.pokemons = POKEMONS;
 
         const id = +this.route.snapshot.paramMap.get('id');
-        for (let i = 0; i < this.pokemons.length; i++) {
-            if (this.pokemons[i].id == id) {
-                this.pokemon = this.pokemons[i];
-            }
-        }
+        this.pokemon = this.pokemonsService.getPokemon(id);
+
     }
 
     goBack(): void {
+
         this.router.navigate(['/pokemons']);
+        
     }
 
 }
